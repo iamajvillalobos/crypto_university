7a2f7fb6697eeb849d91b1fd601f9d297857b450
AJ Villalobos <hi@ajvillalobos.com>
2018-05-11 20:55:41 +0800

    Add Professor resource

---
+++ db/migrate/20180511125113_create_professor.rb
@@ -0,0 +1,9 @@
+class CreateProfessor < ActiveRecord::Migration[5.2]
+  def change
+    create_table :professors do |t|
+      t.string :first_name
+      t.string :last_name
+      t.timestamps
+    end
+  end
+end
