179f6caf973fa13bb0b6c79f3abe61e3c17eead7
AJ Villalobos <hi@ajvillalobos.com>
2018-05-11 21:05:26 +0800

    Add subject resource

---
+++ db/migrate/20180511130220_create_subject.rb
@@ -0,0 +1,9 @@
+class CreateSubject < ActiveRecord::Migration[5.2]
+  def change
+    create_table :subjects do |t|
+      t.string :name
+      t.string :slug
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_11_125600) do
+ActiveRecord::Schema.define(version: 2018_05_11_130220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 2018_05_11_125600) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["professor_id"], name: "index_ratings_on_professor_id"
+  end
+
+  create_table "subjects", force: :cascade do |t|
+    t.string "name"
+    t.string "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
