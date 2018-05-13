5ea06394b2a3ab042b5a2895cb1c156458aa8cb6
AJ Villalobos <hi@ajvillalobos.com>
2018-05-13 13:34:33 +0800

    Add solid brands from fontawesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,4 +4,5 @@ import 'bootstrap'
 import './src/application.scss'
 import fontawesome from '@fortawesome/fontawesome'
 import solid from '@fortawesome/fontawesome-free-solid'
-fontawesome.library.add(solid)
+import brands from '@fortawesome/fontawesome-free-brands'
+fontawesome.library.add(solid, brands)
