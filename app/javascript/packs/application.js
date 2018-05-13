09f48df20abed4230da32d6076abb1acbc41727a
AJ Villalobos <hi@ajvillalobos.com>
2018-05-13 13:11:38 +0800

    Add fontawesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,3 +2,6 @@ console.log('Hello World from Webpacker')
 
 import 'bootstrap'
 import './src/application.scss'
+import fontawesome from '@fortawesome/fontawesome'
+import solid from '@fortawesome/fontawesome-free-solid'
+fontawesome.library.add(solid)
