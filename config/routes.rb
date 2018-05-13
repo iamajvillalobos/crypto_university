0627b1cfc6ea372121760ae59a9e42ba35b4f26c
AJ Villalobos <hi@ajvillalobos.com>
2018-05-13 12:43:22 +0800

    Add classes resource and root url

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  root to: "classes#index"
+  resources :classes, only: [:index, :show]
+
   namespace :admin do
       resources :professors
       resources :ratings
       resources :subjects
-
       root to: "professors#index"
     end
 end
