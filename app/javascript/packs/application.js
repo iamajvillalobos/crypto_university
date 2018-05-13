8224e230c320ef0824532c1f7c900b72e08c50f3
AJ Villalobos <hi@ajvillalobos.com>
2018-05-13 18:47:21 +0800

    Move styles one directory up

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,7 @@
 console.log('Hello World from Webpacker')
 
 import 'bootstrap'
-import './src/application.scss'
+import '../src/application.scss'
 import fontawesome from '@fortawesome/fontawesome'
 import solid from '@fortawesome/fontawesome-free-solid'
 import brands from '@fortawesome/fontawesome-free-brands'
