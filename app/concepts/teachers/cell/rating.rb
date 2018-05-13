e82696fd064dfb52e35e081203dde239e26f265e
AJ Villalobos <hi@ajvillalobos.com>
2018-05-14 00:17:15 +0800

    Rework how average rating is calculated per subject

--- app/concepts/teachers/cell/rating.rb
+++ app/concepts/teachers/cell/rating.rb
@@ -4,13 +4,15 @@ module Teachers
       MAX_RATING = 5
 
       def subject_rating
-        rating = model[:teacher].ratings.find do |r|
-          r.subject_id == model[:course].id
-        end
+        rating = model[:teacher]
+          .ratings
+          .where(subject_id: model[:course].id)
+          .average(:amount)
+          .to_i
 
         return 0 if rating.nil?
 
-        rating.amount
+        rating
       end
 
       def subject_rating_remaining
