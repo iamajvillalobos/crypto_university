f22b3dd934046847ea9adfd8f9c918cc19686737
AJ Villalobos <hi@ajvillalobos.com>
2018-05-14 00:08:48 +0800

    Can create rating for a professor on specific subject

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -7,10 +7,25 @@ class RatingsController < ApplicationController
   end
 
   def create
-    byebug
+    new_rating = Rating.new(
+      amount: rating_params[:amount],
+      description: rating_params[:description],
+      professor: find_teacher,
+      subject: find_subject,
+    )
+
+    if new_rating.save
+      redirect_to class_path(find_subject)
+    else
+      render :new
+    end
   end
 
   private
+
+  def rating_params
+    params.require(:rating).permit(:amount, :description, :professor_id, :class_id)
+  end
 
   def find_teacher
     Professor.find(params[:professor_id])
