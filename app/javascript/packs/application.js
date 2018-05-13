52fa1f6ed98cdc2f4a154f6ff54582b2547fa126
AJ Villalobos <hi@ajvillalobos.com>
2018-05-13 21:34:14 +0800

    Install stimulusjs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,4 +5,10 @@ import '../src/application.scss'
 import fontawesome from '@fortawesome/fontawesome'
 import solid from '@fortawesome/fontawesome-free-solid'
 import brands from '@fortawesome/fontawesome-free-brands'
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
 fontawesome.library.add(solid, brands)
