a2afa00c3a06c4d223b7b85b7ed09e7b993ef526
AJ Villalobos <hi@ajvillalobos.com>
2018-05-13 22:55:57 +0800

    Add description to ratings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_13_082019) do
+ActiveRecord::Schema.define(version: 2018_05_13_144446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2018_05_13_082019) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "subject_id"
+    t.text "description"
     t.index ["professor_id"], name: "index_ratings_on_professor_id"
   end
 
